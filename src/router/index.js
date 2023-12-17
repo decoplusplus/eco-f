80ddaf99ce23f702ac7c7162fcebca612eaf68fa
David Enyoghasim <73113145+enyoghasim@users.noreply.github.com>
2023-12-17 22:45:17 +0100

    added 404 and vercel

--- src/router/index.js
+++ src/router/index.js
@@ -68,6 +68,11 @@ const router = createRouter({
         },
       ],
     },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "404",
+      component: () => import("../views/404.vue"),
+    },
   ],
 });
 
---
+++ src/views/404.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-center min-h-screen text-gray-600 body-font p-5"
+  >
+    <h2 class="md:text-9xl text-6xl font-bold text-red-500 animate-bounce">
+      404
+    </h2>
+    <p class="mb-8 animate-pulse md:text-base text-sm text-center">
+      Oops! The page you are looking for could not be found.
+    </p>
+    <router-link
+      to="/"
+      class="text-white text-base md:text-xl bg-[#00D99D] border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded animate-bounce"
+      >Go Home</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style scoped></style>
